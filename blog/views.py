09a88f60d383cbd34ceec8521578fe1620f30847
luojieemily <luojieemily@foxmail.com>
2018-07-08 11:32:06 +0800

    update my trips

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Blog
+
+def allblogs(request):
+    blogs =Blog.objects.all().order_by('-pub_date')  #all().order_by('-pub_date')为Emily手工添加，为blog按日期排序，但网上有说多了会比较慢
+    return render(request, 'blog/allblogs.html', {'blogs': blogs})
+
+def detail(request, blog_id):
+    detailblog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html',{ 'blog': detailblog})
